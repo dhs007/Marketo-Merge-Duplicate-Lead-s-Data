0e4f786eec508468c3beb92ece5d40deda23e4da
Darshil Shah <shahdarshil_sh@srmuniv.edu.in>
2019-08-29 14:42:58 +0530

    Add files via upload

---
+++ ExceptionController.java
@@ -0,0 +1,35 @@
+package com.tadigital.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/ExceptionController")
+public class ExceptionController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public ExceptionController() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+
+		HttpSession sess = request.getSession();
+		String exception = (String) sess.getAttribute("EXCEPTION");
+
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
