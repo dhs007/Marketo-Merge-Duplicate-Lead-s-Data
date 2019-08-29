75c43ed0ab3ce1321929d55f30f6eafa46082c4c
Darshil Shah <shahdarshil_sh@srmuniv.edu.in>
2019-08-29 14:29:54 +0530

    Add files via upload

---
+++ CleanUpDao.java
@@ -0,0 +1,34 @@
+package com.tadigital.dao;
+
+import com.tadigital.dao.Dao;
+
+import java.io.File;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class CleanUpDao extends Dao {
+	
+	public String deleteFileAndTable ()  {
+		
+		Connection con = openConnection();
+		Statement stmt = openStatement(con);
+		
+		String sql = "DROP TABLE lead_information";
+		try {
+			stmt.executeUpdate(sql);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			
+		}
+		File file = new File("C:\\Users\\darshil.shah\\Desktop\\LeadDataJava.txt");
+		boolean result = file.delete();
+		if(result == false)
+			return "Operation Failed";
+		else
+			return "CleanUp Done Successfully";
+		
+	}
+
+}
